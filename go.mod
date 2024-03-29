8617186fc13bf7e41dde4d29c2a43ae2a8b66397
Adem Baccara <adembc020@gmail.com>
2024-03-29 08:17:12 +0100

    implment graceful shutdown

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/adembc/graceful-shutdown-go
 
 go 1.20
 
-require github.com/go-logr/logr v1.4.1
+require golang.org/x/sync v0.6.0
--- main.go
+++ main.go
@@ -1,41 +1,92 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"golang.org/x/sync/errgroup"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 var (
-	WebPort         = 8080
-	InternalWebPort = 8081
+	WebPort          = 8080
+	InternalWebPort  = 8081
+	interruptSignals = []os.Signal{
+		os.Interrupt,
+		syscall.SIGTERM,
+		syscall.SIGINT,
+	}
 )
 
 func main() {
-
-	go runInternalServer()
-	runMainServer()
+	ctx, stop := signal.NotifyContext(context.Background(), interruptSignals...)
+	defer stop()
+	waitGroup, ctx := errgroup.WithContext(ctx)
+	runInternalServer(waitGroup, ctx)
+	runMainServer(waitGroup, ctx)
+	err := waitGroup.Wait()
+	if err != nil {
+		fmt.Printf("Error during wait: %v\n", err)
+		os.Exit(1)
+	}
 }
 
-func runInternalServer() {
+func runInternalServer(waitGroup *errgroup.Group, ctx context.Context) {
 	internalServer := newInternalServer()
+	waitGroup.Go(func() error {
+		fmt.Printf("Starting internal http server on port : %d\n", InternalWebPort)
+		if err := internalServer.ListenAndServe(); err != nil {
+			if errors.Is(err, http.ErrServerClosed) {
+				return nil
+			}
+			fmt.Printf("Failed to start internal http server: %v\n", err)
+			return err
+		}
+		return nil
 
-	fmt.Printf("Starting internal http server on port : %d\n", InternalWebPort)
-	if err := internalServer.ListenAndServe(); err != nil {
-		fmt.Printf("Failed to start internal http server: %v\n", err)
-		os.Exit(1)
-	}
+	})
+
+	waitGroup.Go(func() error {
+		<-ctx.Done()
+		fmt.Println("Gracefully shutting down internal http server")
+		err := internalServer.Shutdown(context.Background())
+		if err != nil {
+			fmt.Printf("Failed to shutdown internal http server: %v\n", err)
+			return err
+		}
+		fmt.Println("Internal http server shutdown complete")
+		return nil
+	})
 }
 
-func runMainServer() {
+func runMainServer(waitGroup *errgroup.Group, ctx context.Context) {
 	server := newServer()
-
-	fmt.Printf("Starting main http server  on port : %d\n", WebPort)
-	if err := server.ListenAndServe(); err != nil {
-		fmt.Printf("Failed to start main http server: %v\n", err)
-		os.Exit(1)
-	}
+	waitGroup.Go(func() error {
+		fmt.Printf("Starting main http server  on port : %d\n", WebPort)
+		if err := server.ListenAndServe(); err != nil {
+			if errors.Is(err, http.ErrServerClosed) {
+				return nil
+			}
+			fmt.Printf("Failed to start main http server: %v\n", err)
+			return err
+		}
+		return nil
+	})
+	waitGroup.Go(func() error {
+		<-ctx.Done()
+		fmt.Println("Gracefully shutting down main http server")
+		err := server.Shutdown(context.Background())
+		if err != nil {
+			fmt.Printf("Failed to shutdown main http server: %v\n", err)
+			return err
+		}
+		fmt.Println("Main http server shutdown complete")
+		return nil
+	})
 
 }
 
